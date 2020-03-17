a79743899ca20aa143e1f61c2da633d5518816b6
Guilherme Vale <oguilhermevale@gmail.com>
2020-03-17 17:30:06 +0000

    Create footer component, update layout

---
+++ src/components/footer.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+const Footer = () => (
+  <footer>
+    <div>
+      <h1>Interested in working together?</h1>
+      <p>
+        <span>email@simplify.com</span>
+        <span>+351 999 999 999</span>
+      </p>
+    </div>
+  </footer>
+)
+
+export default Footer
--- src/components/header.js
+++ src/components/header.js
@@ -3,31 +3,8 @@ import PropTypes from "prop-types"
 import React from "react"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
+  <header>
+    <nav>{siteTitle}</nav>
   </header>
 )
 
