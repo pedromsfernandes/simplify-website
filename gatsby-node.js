e270c8e12c098c48edebf504b7d4439f58c78a80
Pedro Fernandes <pedromiguel.fernandes17@gmail.com>
2020-03-18 17:20:15 +0000

    add product template and create product pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,4 +4,40 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
+const path = require("path")
+
+exports.createPages = ({ actions, graphql }) => {
+  const { createPage } = actions
+
+  const projectTemplate = path.resolve("src/templates/product.js")
+
+  return graphql(`
+    {
+      allMarkdownRemark {
+        edges {
+          node {
+            html
+            id
+            frontmatter {
+              path
+              name
+            }
+          }
+        }
+      }
+    }
+  `).then(res => {
+    if (res.errors) {
+      return Promise.reject(res.errors)
+    }
+
+    res.data.allMarkdownRemark.edges.forEach(({ node }) => {
+      if (node.frontmatter.path) {
+        createPage({
+          path: node.frontmatter.path,
+          component: projectTemplate,
+        })
+      }
+    })
+  })
+}
---
+++ src/templates/product.js
@@ -0,0 +1,49 @@
+import React from "react"
+import { graphql } from "gatsby"
+import SEO from "../components/seo"
+import Img from "gatsby-image"
+
+export default function Template({ data }) {
+  const { markdownRemark: product } = data
+
+  return (
+    <>
+      <SEO title={product.frontmatter.name} />
+      <div>{product.frontmatter.name}</div>
+      <div>{product.frontmatter.about}</div>
+      <div dangerouslySetInnerHTML={{ __html: product.html }}></div>
+      <Img fluid={product.frontmatter.main_image.childImageSharp.fluid} />
+    </>
+  )
+}
+
+export const productQuery = graphql`
+  query getProduct($path: String!) {
+    markdownRemark(frontmatter: { path: { eq: $path } }) {
+      html
+      id
+      frontmatter {
+        name
+        path
+        about
+        main_image {
+          publicURL
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        carousel {
+          image {
+            childImageSharp {
+              fluid {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
