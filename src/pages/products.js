56a50b53628f2e09deb85a75d55ace16672a5ec7
DIogo Yaguas <diogo.yaguas@hotmail,com>
2020-03-21 15:39:23 +0000

    Finish image animation, typography design and responsive page

--- src/pages/products.js
+++ src/pages/products.js
@@ -8,45 +8,46 @@ import styles from "../styles/products.module.css"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-import ProductCard from "../components/products/ProductCard"
 import data from "../data/productsInformation"
+
+import PlaceholderImg from "../images/placeholder.png"
 
 const ProductsPage = () => (
   <Layout>
     <div className={styles.page}>
       <Container>
         <SEO title="Products" />
-        <h1 className="text-center pt-5">Our Products</h1>
+        <h1 className="main-title text-center pt-5">Our Products</h1>
         <Row className="text-center">
           <Col md="6" className="pt-3">
-            <ProductCard
-              name={data[0].name}
-              description={data[0].description}
-              img={data[0].image}
-            />
+            <div class="product-card">
+              <img src={PlaceholderImg} alt="" />
+              <p className="title text-left pt-1">{data[0].name}</p>
+              <p className="text-left">{data[0].description}</p>
+            </div>
           </Col>
           <Col md="6" className="pt-3">
-            <ProductCard
-              name={data[1].name}
-              description={data[1].description}
-              img={data[1].image}
-            />
+            <div class="product-card">
+              <img src={PlaceholderImg} alt="" />
+              <p className="title text-left pt-1">{data[1].name}</p>
+              <p className="text-left">{data[1].description}</p>
+            </div>
           </Col>
         </Row>
         <Row className="text-center">
           <Col md="6" className="pt-3">
-            <ProductCard
-              name={data[2].name}
-              description={data[2].description}
-              img={data[2].image}
-            />
+            <div class="product-card">
+              <img src={PlaceholderImg} alt="" />
+              <p className="title text-left pt-1">{data[2].name}</p>
+              <p className="text-left">{data[2].description}</p>
+            </div>
           </Col>
           <Col md="6" className="pt-3">
-            <ProductCard
-              name={data[3].name}
-              description={data[3].description}
-              img={data[3].image}
-            />
+            <div class="product-card">
+              <img src={PlaceholderImg} alt="" />
+              <p className="title text-left pt-1">{data[3].name}</p>
+              <p className="text-left">{data[3].description}</p>
+            </div>
           </Col>
         </Row>
       </Container>
