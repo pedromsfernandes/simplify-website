3e592cd275d7a8640f294d1c76b6474277712805
Guilherme Vale <oguilhermevale@gmail.com>
2020-03-21 00:08:14 +0000

    Update landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,10 @@ import Col from "react-bootstrap/Col"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+import HeroArt from "../images/hero-art.png"
+import PlaceholderImg from "../images/placeholder.png"
+import VacImg from "../images/vac1.png"
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
@@ -27,19 +31,73 @@ const IndexPage = () => (
               using technology and multimedia.
             </p>
           </Col>
-          <Col xs={4}>image</Col>
+          <Col xs={4}>
+            <img className="hero-art" src={HeroArt} alt="" />
+          </Col>
         </Row>
       </Container>
     </section>
     <section className="products">
       <Container>
         <h1>Our Products</h1>
+        <Row>
+          <Col>
+            <div class="product-card">
+              <img src={PlaceholderImg} alt="" />
+              <p className="title">Name of the product</p>
+              <p>Description of the product</p>
+            </div>
+          </Col>
+          <Col>
+            <div class="product-card">
+              <img src={PlaceholderImg} alt="" />
+              <p className="title">Name of the product</p>
+              <p>Description of the product</p>
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <div class="product-card">
+              <img src={PlaceholderImg} alt="" />
+              <p className="title">Name of the product</p>
+              <p>Description of the product</p>
+            </div>
+          </Col>
+          <Col>
+            <div class="product-card">
+              <img src={PlaceholderImg} alt="" />
+              <p className="title">Name of the product</p>
+              <p>Description of the product</p>
+            </div>
+          </Col>
+        </Row>
       </Container>
     </section>
     <section className="about-us">
       <Container>
         <h1>About the company</h1>
+        <p>
+          Simplify is a multidisciplinary creative company at the intersection
+          of multimidia, design and technology. Born from a masters disicpline
+          from the University of Porto called Project Management Laboratory,
+          Simplify currently have x collaborators and x active projects.
+        </p>
         <h3>Our clients</h3>
+        <Row>
+          <Col>
+            <img src={VacImg} alt="" />
+          </Col>
+          <Col>
+            <img src={VacImg} alt="" />
+          </Col>
+          <Col>
+            <img src={VacImg} alt="" />
+          </Col>
+          <Col>
+            <img src={VacImg} alt="" />
+          </Col>
+        </Row>
       </Container>
     </section>
     <section className="principles">
@@ -71,7 +129,7 @@ const IndexPage = () => (
           Porto in Portugal.
         </p>
         <p>
-          <Link to="/team">Meet our team members</Link>
+          <Link to="/team">Meet our team</Link>
         </p>
       </Container>
     </section>
