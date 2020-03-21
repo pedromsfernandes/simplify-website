37fe1b5fc77fafff648512a2961782d9135bfcba
DIogo Yaguas <diogo.yaguas@hotmail,com>
2020-03-21 23:37:01 +0000

    Change to markdown files for each product

--- src/pages/products.js
+++ src/pages/products.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Link } from "gatsby"
+import { graphql, Link, useStaticQuery } from "gatsby"
 
 import Container from "react-bootstrap/Container"
 import { Row, Col } from "react-bootstrap"
@@ -9,59 +9,93 @@ import styles from "../styles/products.module.css"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-import data from "../data/productsInformation"
-
 import PlaceholderImg from "../images/placeholder.png"
 
-const ProductsPage = () => (
-  <Layout>
-    <div className={styles.page}>
-      <Container>
-        <SEO title="Products" />
-        <h1 className="main-title text-center pt-5">Our Products</h1>
-        <Row className="text-center">
-          <Col md="6" className="pt-3">
-            <div class="product-card">
-              <Link to="/products/tasker">
-                <img src={PlaceholderImg} alt={data[0].name} />
-                <p className="title text-left pt-1">{data[0].name}</p>
-                <p className="text-left">{data[0].description}</p>
-              </Link>
-            </div>
-          </Col>
-          <Col md="6" className="pt-3">
-            <div class="product-card">
-              <Link to="/products/seats">
-                <img src={PlaceholderImg} alt={data[1].name} />
-                <p className="title text-left pt-1">{data[1].name}</p>
-                <p className="text-left">{data[1].description}</p>
-              </Link>
-            </div>
-          </Col>
-        </Row>
-        <Row className="text-center">
-          <Col md="6" className="pt-3">
-            <div class="product-card">
-              <Link to="/products/environ">
-                <img src={PlaceholderImg} alt={data[2].name} />
-                <p className="title text-left pt-1">{data[2].name}</p>
-                <p className="text-left">{data[2].description}</p>
-              </Link>
-            </div>
-          </Col>
-          <Col md="6" className="pt-3">
-            <div class="product-card">
-              <Link to="/products/neuropal">
-                <img src={PlaceholderImg} alt={data[3].name} />
-                <p className="title text-left pt-1">{data[3].name}</p>
-                <p className="text-left">{data[3].description}</p>
-              </Link>
-            </div>
-          </Col>
-        </Row>
-      </Container>
-    </div>
-  </Layout>
-)
+const getAllProducts = data => {
+  let products = []
+
+  data.allMarkdownRemark.edges.forEach(({ node }) => {
+    products.push({
+      name: node.frontmatter.name,
+      description: node.frontmatter.about,
+      path: node.frontmatter.path,
+    })
+  })
+
+  return products
+}
+
+const ProductsPage = () => {
+  const data = useStaticQuery(graphql`
+    query getProductAndGetProduct {
+      allMarkdownRemark(filter: { fileAbsolutePath: { regex: "/products/" } }) {
+        edges {
+          node {
+            frontmatter {
+              name
+              path
+              about
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  const products = getAllProducts(data)
+
+  return (
+    products.length > 0 && (
+      <Layout>
+        <div className={styles.page}>
+          <Container>
+            <SEO title="Products" />
+            <h1 className="main-title text-center pt-5">Our Products</h1>
+            <Row className="text-center">
+              <Col md="6" className="pt-3">
+                <div class="product-card">
+                  <Link to={products[0].path}>
+                    <img src={PlaceholderImg} alt={products[0].name} />
+                    <p className="title text-left pt-1">{products[0].name}</p>
+                    <p className="text-justify">{products[0].description}</p>
+                  </Link>
+                </div>
+              </Col>
+              <Col md="6" className="pt-3">
+                <div class="product-card">
+                  <Link to={products[1].path}>
+                    <img src={PlaceholderImg} alt={products[1].name} />
+                    <p className="title text-left pt-1">{products[1].name}</p>
+                    <p className="text-justify">{products[1].description}</p>
+                  </Link>
+                </div>
+              </Col>
+            </Row>
+            <Row className="text-center">
+              <Col md="6" className="pt-3">
+                <div class="product-card">
+                  <Link to={products[2].path}>
+                    <img src={PlaceholderImg} alt={products[2].name} />
+                    <p className="title text-left pt-1">{products[2].name}</p>
+                    <p className="text-justify">{products[2].description}</p>
+                  </Link>
+                </div>
+              </Col>
+              <Col md="6" className="pt-3">
+                <div class="product-card">
+                  <Link to={products[3].path}>
+                    <img src={PlaceholderImg} alt={products[3].name} />
+                    <p className="title text-left pt-1">{products[3].name}</p>
+                    <p className="text-justify">{products[3].description}</p>
+                  </Link>
+                </div>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+      </Layout>
+    )
+  )
+}
 
 export default ProductsPage
