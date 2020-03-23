076c86f7ea1f1090c4f1c5ac7bccb7ea72684214
Pedro Fernandes <pedromiguel.fernandes17@gmail.com>
2020-03-23 00:56:27 +0000

    improve product name positioning

--- src/components/products/ProductAbout.js
+++ src/components/products/ProductAbout.js
@@ -6,13 +6,15 @@ import styles from "./Product.module.css"
 
 const ProductAbout = ({ name, image, about }) => (
   <Container className={styles.aboutContainer}>
-    <h1 className={styles.h1}>{name}</h1>
     <Row>
       <Col md="7">
         <Img fluid={image} alt={name} className={styles.image} />
       </Col>
       <Col md="5" style={{ alignSelf: "flex-end" }}>
-        <p className={styles.about}>{about}</p>
+        <div className={styles.aboutWrapper}>
+          <h1 className={styles.h1}>{name}</h1>
+          <p className={styles.about}>{about}</p>
+        </div>
       </Col>
     </Row>
   </Container>
