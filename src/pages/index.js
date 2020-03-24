a1d0c8533950afa7ebee566c6d3c7414100223fa
Pedro Fernandes <pedromiguel.fernandes17@gmail.com>
2020-03-24 11:24:15 +0000

    add missing team member

--- src/pages/index.js
+++ src/pages/index.js
@@ -62,7 +62,7 @@ const IndexPage = () => (
               Simplify is a multidisciplinary creative company at the
               intersection of multimedia, design and technology. Born from a
               masters discipline from the University of Porto called Project
-              Management Laboratory, Simplify currently have 46 collaborators
+              Management Laboratory, Simplify currently have 48 collaborators
               and 4 products.
             </p>
           </Col>
